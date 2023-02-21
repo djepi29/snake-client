6c75f24acc517c653ab0d18947a20531cda245a0
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-02-21 10:46:58 -0500

    post move experiment

--- client.js
+++ client.js
@@ -9,15 +9,25 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // events upon connection
   conn.on('connect', () => {
-    conn.write("Name: JPG")
-  });
-
-   // log message to player upon connection
-   conn.on("connect", () => {
+    // login notice to client
     console.log("Successfully connected to game server");
+
+    // snake initials
+    conn.write("Name: JPG");
+
+    // move the player every 50ms
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    //   conn.write("Move: left");
+    //   conn.write("Move: down");
+    //   conn.write("Move: right");
+    // }, 50);
   });
 
+
+  // data from server
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
