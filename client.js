556c568a72b8217fc323e46f83946ce1b3c852e3
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-02-21 18:21:09 -0500

    interacting with the server

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+// establishes a connection with the game server & events upon connection
+
 const net = require("net");
 
 const connect = function () {
@@ -17,13 +19,6 @@ const connect = function () {
     // snake initials
     conn.write("Name: JPG");
 
-    // move the player every 50ms
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    //   conn.write("Move: left");
-    //   conn.write("Move: down");
-    //   conn.write("Move: right");
-    // }, 50);
   });
 
 
--- play.js
+++ play.js
@@ -1,10 +1,12 @@
 
-const { connect } = require("./client");
-const { setupInput } = require("./input");
 // establishes a connection with the game server
+const { connect } = require("./client");
+
+// setup interface to handle user input from stdin
+const { setupInput } = require("./input");
 
 
 
 console.log("Connecting ...");
-connect();
-setupInput();
+// connect();
+setupInput(connect());
