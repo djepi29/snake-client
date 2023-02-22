89cfe081a929718a77f6621e76260204f8014e57
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-02-21 19:12:03 -0500

    constants file for constants

--- client.js
+++ client.js
@@ -1,11 +1,14 @@
+// constants
+const { IP, PORT } = require("./constants");
+
 // establishes a connection with the game server & events upon connection
 
 const net = require("net");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
+    IP,// IP address here,
+    PORT// PORT number here,
   });
 
   // interpret incoming data as text
